7f64936342e7bee125a1a4fe91a0aa0e755b7dcf
Agustin Beitia <abeitia@uoc.edu>
2022-05-04 16:38:35 -0500

    source files added

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,13 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+ import * as bootstrap from 'bootstrap';
++(function () {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+
+
+})();
+
+

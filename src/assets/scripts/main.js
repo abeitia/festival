28e5b27debd08473e044300871d2a64d83e06065
Agustin Beitia <abeitia@uoc.edu>
2022-05-04 20:47:28 -0500

    ajustes menu

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,12 +2,13 @@
  * Import dependencies from node_modules
  * see commented examples below
  */
+
  import * as bootstrap from 'bootstrap';
-+(function () {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
+// +(function () {
+//   const university = "UOC";
+//   console.log(`Hello, ${university}!`);
 
 
-})();
+// })();
 
 

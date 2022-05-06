709725385e27b91298d39c88ab0bee5c4b0363cc
Agustin Beitia <abeitia@uoc.edu>
2022-05-06 10:38:51 -0500

    theming

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,13 +11,30 @@
 
 })();
 
-var toastTrigger = document.getElementById('liveToastBtn')
-var toastLiveExample = document.getElementById('liveToast')
+var toastTrigger = document.getElementById('liveToastBtn');
+var toastLiveExample = document.getElementById('liveToast');
+
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+var alertTrigger = document.getElementById('tourdatesBtn');
+
 if (toastTrigger) {
 toastTrigger.addEventListener('click', function () {
     var toast = new bootstrap.Toast(toastLiveExample)
     toast.show()
  })
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    alert('Las fechas del tour serán anunciadas próximamente', 'success')
+  })
+}
+
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
 }
 
 
